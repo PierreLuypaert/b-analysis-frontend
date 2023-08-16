78604c9042db9a1bd38529185fc6aa73cfc62bbf
PierreLuypaertRedLab <135008625+PierreLuypaertRedLab@users.noreply.github.com>
2023-08-16 15:54:43 +0100

    feat: create match on front

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { PlayerDetailsComponent } from './components/player-details/player-detai
 import { UploadVideoComponent } from './components/upload-video/upload-video.component';
 import { HttpClientModule } from '@angular/common/http';
 import { DatePipe } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,9 @@ import { DatePipe } from '@angular/common';
   imports: [
     HttpClientModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule 
   ],
   providers: [DatePipe, { provide: LOCALE_ID, useValue: 'fr' }],
   bootstrap: [AppComponent]
